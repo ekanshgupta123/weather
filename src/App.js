4c8a26523001a16b67547262ab6a5388552af864
ekanshgupta123 <ekanshgupta2003@gmail.com>
2022-07-11 13:48:31 -0700

    Our first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {field: ""}
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSumbit = this.handleSumbit.bind(this)
+  }
+
+  handleSumbit(event){
+    const{ field } = this.state
+    this.setState({field})
+
+    let API_Call = `https://api.openweathermap.org/data/2.5/weather?zip=${field},us&appid=384530a26df84465ffe7e9419c8ef8db&units=imperial`
+
+    fetch(API_Call)
+      .then(response => {
+        return response.json()
+      })
+
+      .then(data => {
+        this.setState({
+          city: data.name,
+          temperature : data.main.temp
+        })
+      })
+
+
+      event.preventDefault()
+
+
+  }
+
+  handleChange(event){
+    this.setState({
+        [event.target.name] : event.target.value
+    })
 }
 
+render() {
+  return(
+    <form onSubmit={this.handleSumbit}>
+      <input name = 'field'  type="text" onChange={this.handleChange} placeholder = 'zip code' value = {this.state.field} />
+      <button> Submit </button>
+
+      <div>
+        <h3> City name: {this.state.city}</h3>
+        <h3> Temperature: {this.state.temperature} </h3>
+      </div>
+    </form>
+
+  )
+}
+
+
+
+
+
+
+}
 export default App;
